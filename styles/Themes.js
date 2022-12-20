c4ebfd5ab8d27ecd73903aaeeb0b26ab68987dc6
john smith <awlarpi@gmail.com>
2022-12-20 22:43:21 +0800

    added a white box to the top right

---
+++ styles/Themes.js
@@ -0,0 +1,12 @@
+export const lightTheme = {
+  body: "#000",
+  text: "#000",
+  toggleBorder: "#FFF",
+  background: "#FFF",
+};
+export const darkTheme = {
+  body: "#FFF",
+  text: "#FFF",
+  toggleBorder: "#6B8096",
+  background: "#000",
+};
